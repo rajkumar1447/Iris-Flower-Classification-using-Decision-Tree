974a1c22664ce9d2ee1d5a004ac04e1c917ab67a
rajkumar1447 <rajkumarbetham7@gmail.com>
2025-09-09 11:35:46 +0530

    app.py file included

---
+++ app.py
@@ -0,0 +1,8 @@
+# Install Required and necessary packages.
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
